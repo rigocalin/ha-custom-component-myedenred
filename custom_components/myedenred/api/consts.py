0b60d9e66eb4cda28371d5f282bb1e3baa558c8d
Calin Rigo <rigocalin@gmail.com>
2022-11-07 11:50:13 +0200

    updated url

--- custom_components/myedenred/api/consts.py
+++ custom_components/myedenred/api/consts.py
@@ -1,3 +1,3 @@
-API_LOGIN_URL = "https://www.myedenred.ro/edenred-customer/api/authenticate/default"
-API_LIST_URL = "https://www.myedenred.ro/edenred-customer/api/protected/card/list"
-API_ACCOUNTMOVEMENT_URL = "https://www.myedenred.ro/edenred-customer/api/protected/card/{}/accountmovement"
+API_LOGIN_URL = "https://myedenred.ro/edenred-customer/api/authenticate/default"
+API_LIST_URL = "https://myedenred.ro/edenred-customer/api/protected/card/list"
+API_ACCOUNTMOVEMENT_URL = "https://myedenred.ro/edenred-customer/api/protected/card/{}/accountmovement"
